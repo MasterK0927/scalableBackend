873a7992b75a1d3f98f9a442289dbde37b139e97
MasterK0927 <keshav.agarwal.12325@gmail.com>
2025-08-17 11:48:39 +0530

    chore: update the server to serve the appropriate routes

--- singletonDesignPattern/todoAppSingletonPattern/src/app.ts
+++ singletonDesignPattern/todoAppSingletonPattern/src/app.ts
@@ -1,14 +1,19 @@
 // import
 import express, {Request, Response} from  "express";
+import todoRoutes from "./api/routes/TodoRoutes";
 
 // init
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 // test route
 app.get('/', (req: Request, res: Response) => {
     res.send("server running");
 });
+
+app.use('/api/todos', todoRoutes);
 
 // run
 app.listen(PORT, () => {
